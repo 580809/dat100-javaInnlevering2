b05663181b4b87e4528e0ab140b6244bf8e72231
580809 <brage971@gmail.com>
2024-10-03 19:40:21 +0200

    Update Matriser.java

--- src/main/java/no/hvl/dat100/matriser/Matriser.java
+++ src/main/java/no/hvl/dat100/matriser/Matriser.java
@@ -5,23 +5,57 @@ public class Matriser {
 	// a)
 	public static void skrivUt(int[][] matrise) {
 		
-		// TODO
-		throw new UnsupportedOperationException("Metoden skrivUt ikke implementert");
+		for (int[] i : matrise) {
+			
+			System.out.println();
+			
+			for (int j : i) {
+				
+				System.out.print(j + " ");
+			}
+		}
 	}
 
 	// b)
 	public static String tilStreng(int[][] matrise) {
-
-		// TODO
-		throw new UnsupportedOperationException("Metoden tilStreng ikke implementert");
+		
+		String outStr = ("");
+		
+		for (int[] i : matrise) {
+			
+			for (int j : i) {
+				
+				int v = j;
+				
+				outStr += (v + " ");
+			}
+			
+			outStr += "\n";
+		}
+		
+		return outStr;
 		
 	}
-
 	// c)
 	public static int[][] skaler(int tall, int[][] matrise) {
 		
-		// TODO
-		throw new UnsupportedOperationException("Metoden skaler ikke implementert");
+		int lengde1 = matrise.length;
+		int lengde2 = matrise[0].length;
+		
+		int[][] nyMatrise = new int[lengde1][lengde2];
+		nyMatrise = matrise;
+		
+		for (int i = 0; i < lengde1; i++) {
+			
+			for (int j = 0; j < lengde2; j++) {
+				
+				nyMatrise[i][j] = matrise[i][j] * tall;
+				
+			}
+			
+		}
+		
+		return nyMatrise;
 	
 	}
 
